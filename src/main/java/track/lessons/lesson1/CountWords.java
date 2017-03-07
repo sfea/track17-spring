6ed74d4dee1c27f8548675fa90595516722af089
sfea <sfeaal@gmail.com>
2017-03-07 23:21:19 +0300

    FirstEx

--- src/main/java/track/lessons/lesson1/CountWords.java
+++ src/main/java/track/lessons/lesson1/CountWords.java
@@ -1,6 +1,8 @@
 package track.lessons.lesson1;
 
 import java.io.File;
+import java.io.InterruptedIOException;
+import java.util.Scanner;
 
 /**
  * Задание 1: Реализовать два метода
@@ -27,11 +29,25 @@ public class CountWords {
      * Метод на вход принимает объект File, изначально сумма = 0
      * Нужно пройти по всем строкам файла, и если в строке стоит целое число,
      * то надо добавить это число к сумме
+     *
      * @param file - файл с данными
      * @return - целое число - сумма всех чисел из файла
      */
+
     public long countNumbers(File file) throws Exception {
-        return 0;
+        Scanner sc = new Scanner(file);
+        String sCurrentLine;
+        long sum = 0;
+        while (sc.hasNext()) {
+            String s_next = sc.nextLine();
+            if (s_next.matches("^-?\\d+$")) {
+                sum += Integer.parseInt(s_next);
+            } else {
+                continue;
+            }
+
+        }
+        return sum;
     }
 
 
@@ -39,11 +55,36 @@ public class CountWords {
      * Метод на вход принимает объект File, изначально результат= ""
      * Нужно пройти по всем строкам файла, и если в строка не пустая и не число
      * то надо присоединить ее к результату через пробел
+     *
      * @param file - файл с данными
      * @return - результирующая строка
      */
     public String concatWords(File file) throws Exception {
-        return null;
+        Scanner sc = new Scanner(file);
+        String sCurrentLine;
+        String result = "";
+        while (sc.hasNext()) {
+            String s_next = sc.nextLine();
+            if (!(s_next.matches("^-?\\d+$") || s_next.isEmpty())) {
+                result += s_next + " " ;
+            } else {
+                continue;
+            }
+        }
+            return result;
     }
 
+    public static void main(String[] strings) {
+        String file_name = "/home/fedor/track_java/track17-spring/words.txt";
+        CountWords cw = new CountWords();
+        File file = new File(file_name);
+        try {
+            long cnum = cw.countNumbers(file);
+            String cwords = cw.concatWords(file);
+            System.out.println(cnum);
+            System.out.println(cwords);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
